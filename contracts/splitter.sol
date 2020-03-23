86849ac55958af6e76ad5260599f9f38457a5c8a
SvenRei <sven.reimers@tuhh.de>
2020-03-23 21:17:18 +0100

    :added comments

--- contracts/splitter.sol
+++ contracts/splitter.sol
@@ -1,3 +1,4 @@
+//I think i got some issues with the compiler version..
 pragma solidity ^0.6.0;
 
 //for the correct math with ether/wei
@@ -7,10 +8,10 @@ contract splitter{
     //using the library
     using SafeMath for uint;
    // using safemath for uint;
-    
+
     //saving the addresses into a mapping
     mapping(address => uint) public balance;
-    
+
     //setting up the right addresses on this contract
     modifier CorrectRecipients(address _one, address _two){
         require(_one != _two, "the recpipients can't be the same"); //some guy could go in with the same address
@@ -18,12 +19,12 @@ contract splitter{
         require(_one != address(0x0) && _two != address(0x0), "the addresses can't be 0x0"); //Fake addresses are not allowed
         _;
     }
-    
+
     //just an event for showing the right value splittet
     event rightValue(
         uint value1
         );
-    
+
     // Log who is sending, who are the recipients, which amount will be splittet
     event Balancesplittet(
         address indexed from,
@@ -36,17 +37,17 @@ contract splitter{
         address indexed from,
         uint value2
         );
-        
+
     //setting the function which splits the ether
     //if there are any remainders they're going back to the sender
     function split(address _one, address _two) public payable CorrectRecipients(_one, _two){
         //defining balance as the value sended by the messenger
-        
+
         //the value must be bigger than zero
         require(msg.value > 0, "zero can't be splitted");
         //value mod 2 == 0; might be a useful requirement
         //require(msg.value %2 == 0, "zero can't be splitted");
-        
+
         //division with safemath
         uint tvalue = msg.value.div(2);
         //logging the value
@@ -56,7 +57,7 @@ contract splitter{
         //adding the value to account _one and _two
         balance[_one] = balance[_one] + tvalue;
         balance[_two] = balance[_two] + tvalue;
-        
+
     }
     //Let the user withdraw their ether
     function withdrawTheSplit() public returns(bool done){
@@ -72,5 +73,5 @@ contract splitter{
         msg.sender.transfer(withdraw);
         return true;
     }
-    
+
 }
