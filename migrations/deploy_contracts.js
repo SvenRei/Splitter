6386d77587f3f1b9cbcd0ec096b4ca6f25f435b9
SvenRei <sven.reimers@tuhh.de>
2020-03-24 17:39:19 +0100

    setting to local network

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -1,9 +1,8 @@
-const splitter = artifacts.require("splitter");
-const safemath = artifacts.require("safemath");
-	
-module.exports = function(deployer){
+var splitter = artifacts.require("./splitter.sol");
+var safemath = artifacts.require("./safemath.sol");
 
-  deployer.deploy(splitter);
-  deployer.deploy(safemath);
-  console.log("test");
+module.exports = function (deployer, network, accounts) {
+    deployer.deploy(splitter);
+    deployer.deploy(safemath);
+    console.log("test");
 };
