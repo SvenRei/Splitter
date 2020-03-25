6ed5bc01f8ed07ad39fdd0bd1905ac1b7bef3e40
SvenRei <sven.reimers@tuhh.de>
2020-03-25 23:57:48 +0100

    Switching from function to constructor

--- contracts/owned.sol
+++ contracts/owned.sol
@@ -2,20 +2,22 @@ pragma solidity ^0.5.8;
 
 contract Owned{
 
-     address public owner;
+    address private owner;
 
-     event LogChangeOwner(address indexed sender, address newOwner);
+    event LogChangeOwner(address indexed sender, address newOwner);
+
+    constructor() public {
+      owner = msg.sender;
+    }
 
      modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == owner,"The sender must be the owner");
         _;
     }
 
-    function Owened() public {
-        owner = msg.sender;
-    }
 
     function changeOwner(address newOwner) public onlyOwner returns(bool success){
+        require(newOwner!=address(0x0), "The address of the new owner should not be empty");
         owner = newOwner;
         emit LogChangeOwner(msg.sender, newOwner);
         return true;
