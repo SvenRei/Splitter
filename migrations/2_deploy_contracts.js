1ba7b36dda0fe0f88df1ef1300e056290f85438f
SvenRei <sven.reimers@tuhh.de>
2020-03-26 11:40:26 +0100

    only deploy splitter

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,8 @@
+//only deploy Splitter
 var Splitter = artifacts.require("./Splitter.sol");
-var Safemath = artifacts.require("./Safemath.sol");
-var Owned = artifacts.require("./Owned.sol");
-var Paused = artifacts.require("./Paused.sol");
 
-module.exports = function (deployer, network, accounts) {
-    deployer.deploy(Splitter);
-    deployer.deploy(Safemath);
-	deployer.deploy(Owned);
-	deployer.deploy(Paused);
+module.exports = function(deployer) {
+
+  deployer.deploy(Splitter);
+  
 };
