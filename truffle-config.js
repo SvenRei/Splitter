2f21bf2605ca290b92299f8561b6425119b5f51e
SvenRei <sven.reimers@tuhh.de>
2020-03-26 11:47:45 +0100

    setting config to ganache

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,9 @@
 module.exports = {
   networks: {
-    development: { // This one is optional and reduces the scope for failing fast
-      host: "localhost",
+    development: {
+      host: "127.0.0.1",
       port: 8545,
-      network_id: "*", // Match any network id
-      //gas: 500000
-    },
+      network_id: "*"
+    }
   }
 };
