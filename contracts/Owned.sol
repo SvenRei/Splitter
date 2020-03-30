3363af372e985d1bcb8d8d673c74be60a5394c3f
SvenRei <sven.reimers@tuhh.de>
2020-03-30 13:41:50 +0200

    Renaming done

---
+++ contracts/Owned.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.8;
+
+contract Owned{
+
+    address private owner;
+
+    event LogChangeOwner(address indexed sender, address indexed newOwner);
+
+    constructor() public {
+      owner = msg.sender;
+    }
+
+     modifier onlyOwner {
+        require(msg.sender == owner,"The sender must be the owner");
+        _;
+    }
+
+
+    function changeOwner(address newOwner) public onlyOwner returns(bool success){
+        require(newOwner != address(0x0), "The address of the new owner should not be empty");
+        owner = newOwner;
+        emit LogChangeOwner(msg.sender, newOwner);
+        return true;
+    }
+
+}
