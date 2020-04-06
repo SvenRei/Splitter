741beafb08d750e5dee20552a8c5f6182364b238
SvenRei <sven.reimers@tuhh.de>
2020-04-06 09:54:24 +0200

    adding amount to withdraw

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.8;
 
 //for pausing
-import "./Paused.sol";
+import "./Killed.sol";
 //for the correct math with ether/wei
 import "./Safemath.sol";
 
 
-contract Splitter is Paused{
+contract Splitter is Killed{
     //using the library
     using SafeMath for uint;
    // using safemath for uint;
@@ -22,21 +22,19 @@ contract Splitter is Paused{
         uint sendAmount
         );
     //record who is withdrawing ether/wei
-    event LogWithdrew(
+    event LogWithdraw(
         address indexed from,
-        uint remainder
+        uint amount
         );
 
     //setting the function which splits the ether
     //if there are any remainders they're going back to the sender
     function split(address one, address two) public payable whenRunning {
+        require(one != address(0x0) && two != address(0x0), "the addresses can't be 0x0"); //Fake addresses are not allowed
         require(one != two, "the recpipients can't be the same"); //some guy could go in with the same address
         require(msg.sender != one && msg.sender != two, "the sender can't be a recipient"); //ALice isn't allowed to get money
-        require(one != address(0x0) && two != address(0x0), "the addresses can't be 0x0"); //Fake addresses are not allowed
-        //defining balance as the value sended by the messenger
-
         //the value must be bigger than zero
-        require(msg.value > 0, "zero can't be splitted");
+        require(msg.value > 2, "At least two Wei should be splittet");
 
         //division with safemath
         uint half = msg.value.div(2);
@@ -54,22 +52,15 @@ contract Splitter is Paused{
 
     }
     //Let the user withdraw their ether
-    function withdraw() public whenRunning returns(bool done){
-        //saving a local variable
-        uint amount = balances[msg.sender];
-        //it must be bigger than zero
+    function withdraw(uint amount) public whenRunning returns(bool done){
         require(amount > 0, "A higher balance than zero, is a prerequisite");
+        require(amount <= balances[msg.sender], "amount > balance[msg.sender]");
         //the balance set to 0 because when calling again the balance would be bigger
-        balances[msg.sender] = 0;
+        balances[msg.sender] = balances[msg.sender] - amount;
         //logging the Withdraw
-        emit LogWithdrew(msg.sender, amount);
+        emit LogWithdraw(msg.sender, amount);
         //transferring the money to the accounts
         msg.sender.transfer(amount);
         return true;
     }
-    //getBalance
-    function getBalance(address stateBalance) public view returns(uint) {
-       return balances[stateBalance];
-    }
-
 }
