688ddcaaf85b4a38bd45966fd32e59584298b1b9
SvenRei <sven.reimers@tuhh.de>
2020-05-15 13:44:34 +0200

    fix balance bug & adding withdraw

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,9 @@ const Web3 = require("web3");
 const truffleContract = require("truffle-contract");
 const $ = require("jquery");
 // Not to forget our built contract
-const splitterJson = require("../../build/contracts/Splitter.json")
+const splitterJson = require("../../build/contracts/Splitter.json");
 
+require("file-loader?name=../index.html!../index.html");
 
 if (typeof web3 !== 'undefined') {
     // Use the Mist/wallet/Metamask provider.
@@ -16,154 +17,196 @@ if (typeof web3 !== 'undefined') {
 const Splitter = truffleContract(splitterJson);
 Splitter.setProvider(web3.currentProvider);
 
-//accounts
-let sender = {address: {key: "#senderAddress", val: "0x"}, balance: {key: "#senderBalance", val: 0}};
-let one = {address: {key: "#oneAddress", val: "0x"}, balance: {key: "#oneBalance", val: 0}};
-let two = {address: {key: "#twoAddress", val: "0x"}, balance: {key: "#twoBalance", val: 0}};
+window.addEventListener('load', async function() {
+    try {
+        const accounts = await (
+          web3.eth.getAccounts());
+        if (accounts.length == 0) {
+            $("senderBalance").html("N/A");
+            throw new Error("No account with which to transact");
+        }
 
-//add listener
-window.addEventListener('load', function() {
-  return web3.eth.getAccounts()
-        .then(accounts => {
-          //must be sender, one, two or more
-          if (accounts.length < 2) {
-             for (const acc of [sender, one, two]) {
-                 $(acc.address.key).html("N/A");
-                 $(acc.balance.key).html("N/A");
-             }
-            //error handling
-             throw new Error("No required accounts present");
-          }
-          //setting acc
-          sender.address.val = accounts[0];
-          one.address.val = accounts[1];
-          two.address.val = accounts[2];
-          return Splitter.deployed();
-        })
-        //show balances at the beginning
-        .then(deployed => {
-          //refresh the balances
-          refreshAllBalances([sender, one, two]);
-          //set the buttons
-          $("#split").click(split);
-          $("#refresh").click(refresh);
-      })
+        //network
+        const network = await web3.eth.net.getId();
+        console.log("Network:", network.toString(10));
+        //deploy
+        const deployed = await Splitter.deployed();
+        console.log(deployed);
 
-       .catch(error => {
-           $("#status").html(error.toString());
-           console.error(error);
-    });
-  });
-  //refresh the balances (refreshing the balances of one and two after split is buggy..)
-  //refresh all! balances
-  function refreshAllBalances(accounts) {
-      //status
-      $("#status").html("Refreshing balances...");
-      //helper
-      let counter = 0;
-      //get the length of the array
-      const accNumber = accounts.length;
-      //log
-      console.log("length" + accounts.length);
-      //set the function for calling
-      function refreshBalance(account) {
-          //get the balance!
-          function getBalance(account) {
-              //return the balance
-              return web3.eth.getBalance(account.address.val)
-              //update the balance
-              .then(bal => {
-                  //saving the balance
-                  account.balance.val = bal;
-                  //log the balance
-                  console.log("Balance of acc" + "\t" + counter + "\t" + bal);
-                  //update
-                  $(account.balance.key).html(account.balance.val);
-                  //helper ++
-                  counter++;
-                  //console.log("counter" + counter);
-                  if (counter == accNumber) {
+        //set up sender
+        const sender = accounts[0];
+        //log the sender
+        console.log("Sender:", sender);
+        let senderBalance = await deployed.balances(sender, {from: sender});
+        $("#senderBalance").html(senderBalance.toString(10));
+        console.log("sender:", senderBalance);
 
-                      $("#status").html("Balances refreshed");
-                  }
-              })
-              //error handling
-              .catch(error => {
-                  $("#status").html(error.toString());
-                  console.error(error);
-              });
-          }
-          //update
-          $(account.address.key).html(account.address.val);
-          //call function getBalance
-          getBalance(account);
+        //set up account1
+        const one = accounts[1];
+        //log acc one
+        console.log("One:", one);
+        let oneBalance = await deployed.balances(one, {from: one});
+        $("#oneBalance").html(oneBalance.toString(10));
 
-      };
-      //iterating
-      for (const acc of accounts) {
-          refreshBalance(acc);
-          //logging
-          console.log("this is the account___:" + acc.balance.key );
-      }
+        //set up account2
+        const two = accounts[2];
+        //log acc two
+        console.log("Two:", two);
+        let twoBalance = await deployed.balances(two, {from: two});
+        $("#twoBalance").html(twoBalance.toString(10));
+
+       // We wire it when the system looks in order.
+       $("#split").click(split);
+       $("#withdraw").click(withdraw);
+
+
+       } catch(err) {
+         // Never let an error go unlogged.
+         console.error(err);
   }
+});
 
 //set up split function
-const split = function() {
-  //declaring local
-  let deployed;
-  //gas
+const split = async function() {
+
+  //setting gas
   const gas = 300000;
-  //amount from the input
-  const amount = $("input[name='amount']").val();
-  //log
-  console.log(amount);
-  //get into splitter
-  return Splitter.deployed()
-      .then(_deployed => {
-          deployed = _deployed;
 
-          /// We simulate the real call and see whether this is likely to work.
-          // No point in wasting gas if we have a likely failure.
-          return deployed.split.call(one.address.val, two.address.val,
-              { from: sender.address.val, value: amount, gas: gas });
-      })
-      .then(success => {
-          if (!success) {
-              throw new Error("The transaction will fail anyway, not sending");
-          }
-          //  proper transaction.
-          return deployed.split(one.address.val, two.address.val,
-              { from: sender.address.val, value: amount, gas: gas })
-              .on(
-                  "transactionHash",
-                  txHash => $("#status").html("Transaction on the way " + txHash)
-              );
-      })//for browser console..
-      .then(txObj => {
-                 const receipt = txObj.receipt;
-                 console.log("got receipt", receipt);
-                 if (!receipt.status) {
-                     console.error("Wrong status");
-                     console.error(receipt);
-                     $("#status").html("There was an error in the tx execution, status not 1");
-                 } else if (receipt.logs.length == 0) {
-                     console.error("Empty logs");
-                     console.error(receipt);
-                     $("#status").html("There was an error in the tx execution, missing expected event");
-                 } else {
-                     console.log(receipt.logs[0]);
-                     $("#status").html("Transfer executed");
-                 }
-             })
+  try{
+    const accounts = await(
+      web3.eth.getAccounts());
+    if(accounts.length==0){
+      $("#balance").html("N/A");
+      throw new Error("no accounts");
+    }
+    //setting acc
+    const deployed = await Splitter.deployed();
+    const [sender, one, two] = accounts;
+
+    /// We simulate the real call and see whether this is likely to work.
+    // No point in wasting gas if we have a likely failure.
+    const success = await deployed.split.call(
+           $("input[name='recipient_one']").val(),
+           $("input[name='recipient_two']").val(),
+           // Giving a string is fine
+           { from: sender, value: $("input[name='amount']").val(), gas: gas });
+    if(!success){
+      throw new Error("doens't work");
+    }
+    // Ok, we move onto the proper action.
+        const txObj = await deployed.split(
+            $("input[name='recipient_one']").val(),
+            $("input[name='recipient_two']").val(),
+            // Giving a string is fine
+            { from: sender, value: $("input[name='amount']").val(), gas: gas })
+            //split takes time in real life, so we get the txHash immediately while it
+            // is mined.
+            .on(
+                "transactionHash",
+                txHash => $("#status").html("Transaction on the way " + txHash)
+            );
+        // Now we got the mined tx.
+        const receipt = txObj.receipt;
+        console.log("got receipt", receipt);
+        if (!receipt.status) {
+            console.error("Wrong status");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, status not 1");
+        } else if (receipt.logs.length == 0) {
+            console.error("Empty logs");
+            console.error(receipt);
+            $("#status").html("There was an error in the tx execution, missing expected event");
+        } else {
+            console.log(receipt.logs[0]);
+            $("#status").html("Transfer executed");
+        }
+        // Make sure we update the UI.
+        //set up sender
+        let senderBalance = await deployed.balances(sender, {from: sender});
+        $("#senderBalance").html(senderBalance.toString(10));
+
+        //set up account1
+        let oneBalance = await deployed.balances(one, {from: one});
+        $("#oneBalance").html(oneBalance.toString(10));
+
+        //set up account2
+        let twoBalance = await deployed.balances(two, {from: two});
+        $("#twoBalance").html(twoBalance.toString(10));
+
+       //error handling
+     } catch(err) {
+         $("#status").html(err.toString());
+         console.error(err);
+       }
+   };
+//set up split function
+const withdraw = async function() {
+
+ //gas
+ const gas = 300000;
+
+ try{
+   const accounts = await(
+     web3.eth.getAccounts());
+   if(accounts.length==0){
+     $("#balance").html("N/A");
+     throw new Error("no accounts");
+   }
+
+   const deployed = await Splitter.deployed();
+   const [sender, one, two] = accounts;
+
+   /// We simulate the real call and see whether this is likely to work.
+   // No point in wasting gas if we have a likely failure.
+   const success = await deployed.withdraw.call(
+          $("input[name='withdrawAmount']").val(),
+          // Giving a string is fine
+          { from: $("input[name='recipient']").val(), gas: gas });
+   if(!success){
+     throw new Error("doens't work");
+   }
+   // Ok, we move onto the proper action.
+       const txObj = await deployed.withdraw(
+             $("input[name='withdrawAmount']").val(),
+           // Giving a string is fine
+           { from: $("input[name='recipient']").val(),  gas: gas })
+           //split takes time in real life, so we get the txHash immediately while it
+           // is mined.
+           .on(
+               "transactionHash",
+               txHash => $("#status").html("Transaction on the way " + txHash)
+           );
+       // Now we got the mined tx.
+       const receipt = txObj.receipt;
+       console.log("got receipt", receipt);
+       if (!receipt.status) {
+           console.error("Wrong status");
+           console.error(receipt);
+           $("#status").html("There was an error in the tx execution, status not 1");
+       } else if (receipt.logs.length == 0) {
+           console.error("Empty logs");
+           console.error(receipt);
+           $("#status").html("There was an error in the tx execution, missing expected event");
+       } else {
+           console.log(receipt.logs[0]);
+           $("#status").html("Transfer executed");
+       }
+       // Make sure we update the UI.
+       //set up sender
+       let senderBalance = await deployed.balances(sender, {from: sender});
+       $("#senderBalance").html(senderBalance.toString(10));
+
+       //set up account1
+       let oneBalance = await deployed.balances(one, {from: one});
+       $("#oneBalance").html(oneBalance.toString(10));
+
+       //set up account2
+       let twoBalance = await deployed.balances(two, {from: two});
+       $("#twoBalance").html(twoBalance.toString(10));
+
       //error handling
-      .catch(error => {
-          $("#status").html(error.toString());
-          console.error(error);
-      });
-};
-//for calling.. which is not working..^^
-const refresh = function() {
-    refreshAllBalances([sender, one, two]);
-};
-
-require("file-loader?name=../index.html!../index.html");
+    } catch(err) {
+        $("#status").html(err.toString());
+        console.error(err);
+      }
+  };
